0a5fb6a62f3f0807621b190bb6e4a3eff2bfce98
bclauson <clauson.branden@gmail.com>
2021-11-15 20:20:46 -0800

    pass test for numbers 1-3 and 5-8
    co-authored-by: Elliot McGonigal <elliot@elliotmcgonigal.com>

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ Test: If we input "1", "2", or "3", we want to display "I", "II", or "III"
 Code: romanNumerals(1); romanNumerals(2); romanNumerals(3);
 <br>
 Expect:"I"; "II"; "III"
+
+<br>
+Test: If we input '5', we want to display 'V'
+<br>
+Code: romanNumerals(5);
+<br>
+Expect: Expect: 'V'
--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,35 @@ function romanNumerals(number) {
   let result = "";
   userInput = parseInt(number);
 
-  for (let i = 0; i < userInput; i++) {
-    result = result.concat(symbols[0]);
-    
+  if (userInput <= 3) {
+    for (let i = 0; i < userInput; i++) {
+      result = result.concat(symbols[0]);
+      
+    }
+  }
+
+  else if (userInput <= 8) {
+    result = result.concat(symbols[1]);
+    for (let i = 0; i < (userInput - 5); i++) {
+      result = result.concat(symbols[0]);
+    }
   }
 
   
   return result;
 }
-
+ 
 /*
-I
-II
-III
-IV
-V
-VI
-VII
-VIII
-IX
-X
+I  symbol[0]     X  symbol[2]
+II      XX
+III     XXX if number <= 3
+IV      XL if == 4
+V   symbol[1]   L   symbol[3]if number <= 8
+VI      LX
+VII     LXX
+VIII    LXXX
+IX       XC if == 9
+X     symbol[2]    C  symbol[4]
  concat(symbols[0],symbols[2]) + concat(symbols[2],symbols[4])
 IX
 XC 
